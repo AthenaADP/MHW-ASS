bd42395ce6bfc49e76c566b3fa11df744f2ffd21
AthenaADP <hownottobeseen@gmail.com>
2018-06-12 21:46:24 +1200

    Added Kirin Gamma armor

--- Version.h
+++ Version.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define VERSION_NO 0.43b
-#define VERSION_RC 0,4,3,1
+#define VERSION_NO 0.44b
+#define VERSION_RC 0,4,4,1
 
 #define STRINGIZE2(X) #X
 #define STRINGIZE(X) STRINGIZE2(X)
