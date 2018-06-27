2f70c2cf33e68e6dec30df0bfc741514e6cb8f5c
AthenaADP <hownottobeseen@gmail.com>
2018-06-27 21:27:29 +1200

    Updated version number to 0.46b

--- Version.h
+++ Version.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define VERSION_NO 0.45b
-#define VERSION_RC 0,4,5,1
+#define VERSION_NO 0.46b
+#define VERSION_RC 0,4,6,1
 
 #define STRINGIZE2(X) #X
 #define STRINGIZE(X) STRINGIZE2(X)
