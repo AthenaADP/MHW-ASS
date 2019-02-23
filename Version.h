d96a83059ce921535ffa784da73d182995f356a3
AthenaADP <hownottobeseen@gmail.com>
2019-02-23 22:03:22 +1300

    Reverted accidental commit

--- Version.h
+++ Version.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define VERSION_NO 0.61b
-#define VERSION_RC 0,6,1,1
+#define VERSION_NO 0.60b
+#define VERSION_RC 0,6,0,1
 
 #define STRINGIZE2(X) #X
 #define STRINGIZE(X) STRINGIZE2(X)
